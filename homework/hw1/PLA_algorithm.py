72ede6da9f2761ab3c1727a918f8902f01632174
Offliners <eric21904@gmail.com>
2021-10-27 22:49:55 +0800

    q17

--- homework/hw1/PLA_algorithm.py
+++ homework/hw1/PLA_algorithm.py
@@ -31,3 +31,36 @@ class PLA_naive_cycle:
                 break
         
         return T
+
+class PLA_naive_cycle_with_LR:
+    """
+        Initial weight w is 0
+        Take sign(0) = -1
+        You can set your own learning rate.
+        Halt condition is that all datas are classified correctly.
+        This program will return update times (T) of PLA in naive cycle order. 
+    """
+    def __init__(self, lr):
+        self.lr = lr
+
+    def run(self, X, y):
+        w = np.zeros(X.shape[1])
+        T = 0
+        while True:
+            correct = 0
+            for i in range(X.shape[0]):
+                if np.sign(w.T.dot(X[i])) == 0.0:
+                    sign = -1
+                else:
+                    sign = np.sign(w.T.dot(X[i]))
+
+                if sign != y[i]:
+                    w += self.lr * y[i] * X[i]
+                    T += 1
+                else:
+                    correct += 1
+
+            if correct == X.shape[0]:
+                break
+        
+        return T
---
+++ homework/hw1/q17.py
@@ -0,0 +1,22 @@
+import numpy as np
+from util import readFile, addW0, unison_shuffled_copies
+from PLA_algorithm import PLA_naive_cycle_with_LR
+
+filePath = 'homework\hw1\data\hw1_15_train.dat'
+
+X, y = readFile(filePath)
+X = addW0(X)
+
+print(X.shape)
+print(y.shape)
+
+pla_lr = PLA_naive_cycle_with_LR(0.5)
+
+T_sum = 0
+expTimes = 2000
+for i in range(expTimes):
+    np.random.seed(i)
+    X, y = unison_shuffled_copies(X, y)
+    T_sum += pla_lr.run(X, y)
+
+print('Average update times : ' + str(T_sum / 2000))
