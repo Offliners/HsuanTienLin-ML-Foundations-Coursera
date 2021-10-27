6a0991e9b02837da28d7f2ab09a9ef7c36b4bb25
Offliners <eric21904@gmail.com>
2021-10-27 17:13:53 +0800

    fixed some bugs

--- homework/hw1/q15.py
+++ homework/hw1/q15.py
@@ -2,7 +2,7 @@ import numpy as np
 from util import readFile, addW0
 from PLA_algorithm import PLA_naive_cycle
 
-filePath = 'hw1\data\hw1_15_train.dat'
+filePath = 'homework\hw1\data\hw1_15_train.dat'
 
 X, y = readFile(filePath)
 X = addW0(X)
@@ -11,4 +11,4 @@ print(X.shape)
 print(y.shape)
 
 pla = PLA_naive_cycle()
-print('Update time : ' + str(pla.run(X, y)))
+print('Update times : ' + str(pla.run(X, y)))
